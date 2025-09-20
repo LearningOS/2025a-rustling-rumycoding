1b636c580e428b046ed1263e104d73f9b6807bdb
rumycoding <rumycoding@outlook.com>
2025-09-20 22:22:12 +0800

    do exec

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -16,12 +16,25 @@
 //
 // Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+use std::fmt;
+pub enum Grade {
+    Number(f32),
+    Alphabet(&'static str)
+}
 
 pub struct ReportCard {
-    pub grade: f32,
+    pub grade: Grade,
     pub student_name: String,
     pub student_age: u8,
+}
+
+impl fmt::Display for Grade {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self{
+            Grade::Alphabet(a) => write!(f, "{}", a),
+            Grade::Number(a) => write!(f, "{}", a),
+        }
+    }
 }
 
 impl ReportCard {
@@ -38,7 +51,7 @@ mod tests {
     #[test]
     fn generate_numeric_report_card() {
         let report_card = ReportCard {
-            grade: 2.1,
+            grade: Grade::Number(2.1),
             student_name: "Tom Wriggle".to_string(),
             student_age: 12,
         };
@@ -52,7 +65,7 @@ mod tests {
     fn generate_alphabetic_report_card() {
         // TODO: Make sure to change the grade here after you finish the exercise.
         let report_card = ReportCard {
-            grade: 2.1,
+            grade: Grade::Alphabet("A+"),
             student_name: "Gary Plotter".to_string(),
             student_age: 11,
         };
